21ce11a8ae7aa0ea8b31bceca0e8ff626b513e44
Mat2dr <dragovic.mathias@gmail.com>
2023-02-06 11:01:57 +0100

    Table pagination

--- hrnet/src/components/TableData/TableData.jsx
+++ hrnet/src/components/TableData/TableData.jsx
@@ -1,5 +1,5 @@
 import React, {useMemo} from 'react'
-import { useTable, useSortBy, useGlobalFilter } from 'react-table'
+import { useTable, useSortBy, useGlobalFilter, usePagination } from 'react-table'
 import EMPLOYEES_DATA from '../../Data/mockEmployees';
 import COLUMNS from '../TableData/columns'
 import { GlobalFilter } from './GlobalFilter';
@@ -17,17 +17,25 @@ const TableData = () => {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        rows,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
         prepareRow,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
         state,
         setGlobalFilter,
       } = useTable({
         columns,
         data
       },
-       useGlobalFilter, useSortBy)
+       useGlobalFilter, useSortBy, usePagination)
 
-       const { globalFilter } = state;
+       const { globalFilter, pageIndex, pageSize } = state;
 
   return (
     <div>
@@ -52,7 +60,7 @@ const TableData = () => {
                 }
             </thead>
             <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
+                {page.map(row => {
                     prepareRow(row)
                     return (
                         <tr className='row' {...row.getRowProps()}>
@@ -64,6 +72,36 @@ const TableData = () => {
                 })}
             </tbody>
         </table>
+        <div>
+            <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+                {
+                    [10,25,50,100].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))
+                }
+            </select>
+            <span>
+                Page{' '}
+                <strong>
+                    { pageIndex + 1} of {pageOptions.length} 
+                </strong>
+            </span>
+            <span>
+                | Go to page: {' '}
+                <input type='number' defaultValue={pageIndex + 1} 
+                onChange={e => {
+                    const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                    gotoPage(pageNumber)
+                }}
+                style={{width: '50px'}} />
+            </span>
+            <button onClick={ ()=> gotoPage(0) } disabled={!canPreviousPage}>{'<<'}</button>
+            <button onClick={ ()=> previousPage() } disabled={!canPreviousPage}>Previous</button>
+            <button onClick={ ()=> nextPage() } disabled={!canNextPage}>Next</button>
+            <button onClick={ ()=> gotoPage(pageCount - 1) } disabled={!canNextPage}>{'>>'}</button>
+        </div>
     </div>
     
   )
